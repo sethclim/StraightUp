75eaf46d7581723aeb13ee5f16371bf63d7344d9
Shae <shaebsimeoni@gmail.com>
2023-11-27 17:49:56 -0500

    Added MediaPipe model and basic video accessing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const getVideo = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 1280, height: 720 },
+      });
+
+      let video = videoRef.current;
+      if (video) {
+        video.srcObject = stream;
+        video.play();
+      }
+    } catch (error) {
+      console.error("Error accessing camera:", error);
+    }
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Straight Up!</p>
+        <video ref={videoRef}></video>
       </header>
     </div>
   );
